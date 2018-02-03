d1030a9d22265bf2d316f1043ea1117605c0ee37
Sisi R <sisir@andrew.cmu.edu>
2018-02-03 18:08:17 -0500

    Update static file and error handling
    1. Update link to static files
    2. add exception handling

--- hw3/README.md
+++ hw3/README.md
@@ -1,3 +1,5 @@
 Please put hw3 files in this directory.
 
+This is a Django calculator application
 Python 2.7.10
+
--- hw3/calculator/views.py
+++ hw3/calculator/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import sys
 
 # Create your views here.
 def home(request):
@@ -19,78 +20,104 @@ def home(request):
 	error = ''
 
 	context = {'display': 0}
+	try:
 	#initialize variables if there's a client-side value
-	if 'new_val' in request.POST and request.POST['new_val'] != '':
-		new_val = int(request.POST['new_val'])
-		context['new_val'] = new_val
+		if 'new_val' in request.POST and request.POST['new_val'] != '':
+			new_val = int(request.POST['new_val'])
+			context['new_val'] = new_val
 
-	if 'prevInputIsNum' in request.POST and request.POST['prevInputIsNum'] != '':
-		prevInputIsNum = (request.POST['prevInputIsNum'] == 'True')
-		context['prevInputIsNum'] = prevInputIsNum
+		if 'prevInputIsNum' in request.POST and request.POST['prevInputIsNum'] != '':
+			prevInputIsNum = (request.POST['prevInputIsNum'] == 'True')
+			context['prevInputIsNum'] = prevInputIsNum
 
-	if 'prev_value' in request.POST and request.POST['prev_value'] != '':
-		prev_value = int(request.POST['prev_value'])
-		context['prev_value'] = prev_value
+		if 'prev_value' in request.POST and request.POST['prev_value'] != '':
+			prev_value = int(request.POST['prev_value'])
+			context['prev_value'] = prev_value
 
-	if 'prev_oper' in request.POST and request.POST['prev_oper'] != '':
-		prev_oper = request.POST['prev_oper']
-		context['prev_oper'] = prev_oper
-	
-	#------------processing-------------------------------
-	if 'number' in request.POST:
-		input = int(request.POST['number'])
-		context['prevInputIsNum'] = True;
-		new_val = new_val * 10+ input
-		context['display'] = new_val
-		context['new_val'] = new_val
+		if 'prev_oper' in request.POST and request.POST['prev_oper'] != '':
+			prev_oper = request.POST['prev_oper']
+			context['prev_oper'] = prev_oper
+		
+		#------------processing-------------------------------
+		if 'number' in request.POST:
+			input = int(request.POST['number'])
+			if not isinstance(input, int):
+				error = "Input should be 0~9!!"
+				new_val = 0
+				prev_value = 0
+				prev_oper = '+'
+				context['error'] = error
+				context['new_val'] = new_val
+				context['prev_value'] = prev_value
+				context['prev_oper'] = prev_oper
+			else:
+				context['prevInputIsNum'] = True;
+				new_val = new_val * 10+ input
+				context['display'] = new_val
+				context['new_val'] = new_val
 
 
-	if 'operator' in request.POST:
-		input = request.POST['operator']
+		if 'operator' in request.POST:
+			input = request.POST['operator']
 
-		flag = False;
-		# print "prevInputIsNum",prevInputIsNum
-		if prevInputIsNum:
-			if prev_oper == '+':
-				prev_value = prev_value + new_val
+			flag = False;
+			# print "prevInputIsNum",prevInputIsNum
+			if prevInputIsNum:
+				if prev_oper == '+':
+					prev_value = prev_value + new_val
 
-			elif prev_oper == '-':
-				prev_value = prev_value - new_val
+				elif prev_oper == '-':
+					prev_value = prev_value - new_val
 
-			elif prev_oper == '*':
-				prev_value = prev_value * new_val
+				elif prev_oper == '*':
+					prev_value = prev_value * new_val
 
-			elif prev_oper == '/':
-				if new_val == 0:
-					error = "Can't divide by 0!"
+				elif prev_oper == '/':
+					if new_val == 0:
+						error = "Can't divide by 0!"
+						new_val = 0
+						prev_value = 0
+						prev_oper = '+'
+						flag = True
+					else:
+						prev_value = prev_value // new_val
+
+				elif prev_oper == '=':
+					prev_value = new_val
+
+				else:
+					error = "Invalid input!!"
 					new_val = 0
 					prev_value = 0
 					prev_oper = '+'
 					flag = True
-				else:
-					prev_value = prev_value // new_val
 
-			elif prev_oper == '=':
-				prev_value = new_val
+			# update value and send to client side
+			if not flag:
+				# print "is not flag",flag
+				# print "input",input
+				prev_oper = input
+			context['display'] = prev_value
+			context['prevInputIsNum'] = False
+			context['new_val'] = 0
+			context['prev_oper'] = prev_oper
+			context['prev_value'] = prev_value
+			context['error'] = error
 
-		# update value and send to client side
-		if not flag:
-			# print "is not flag",flag
-			# print "input",input
-			prev_oper = input
-		context['display'] = prev_value
-		context['prevInputIsNum'] = False
-		context['new_val'] = 0
-		context['prev_oper'] = prev_oper
-		context['prev_value'] = prev_value
-		context['error'] = error
+		if 'clear' in request.POST:
+			context['new_val'] = 0
+			context['prev_value'] = 0
+			context['prev_oper'] = '+'
+			context['prevInputIsNum'] = False
+			context['display'] = 0
 
-	if 'clear' in request.POST:
+	except:
+		context['error'] = "Error: Invalid input!"
 		context['new_val'] = 0
 		context['prev_value'] = 0
 		context['prev_oper'] = '+'
-		context['prevInputIsNum'] = False
-		context['display'] = 0
+        # print out error message
+        print(sys.exc_info())
 
 
 	return render(request, 'calculator/calculator.html', context)
