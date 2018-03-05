63e9532f46d210f45ade6a9bba75b16f555baf4e
Sisi R <sisir@andrew.cmu.edu>
2018-03-04 22:19:11 -0500

    Initial submit
    Add email authentication:
    sending e-mail from web app and generated inks in the e-mail that return to the web app.

---
+++ hw7/socialnetwork/views.py
@@ -0,0 +1,290 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.utils import timezone
+from django.db import transaction
+
+from socialnetwork.forms import *
+from socialnetwork.models import *
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.core import serializers
+
+# Used to generate a one-time-use token to verify a user's email address
+from django.contrib.auth.tokens import default_token_generator
+
+# Used to send mail from within Django
+from django.core.mail import send_mail
+
+
+
+
+
+@login_required
+@ensure_csrf_cookie
+def home(request):
+    posts = Post.objects.all().order_by("-creation_time")
+    comments = Comment.objects.all().order_by("-created_time")
+
+    context = {'posts':posts, 'comments':comments}
+    return render(request, 'socialnetwork/global.html', context)
+
+
+@transaction.atomic
+def register(request):
+    context = {}
+
+    # Just display the registration form if this is a GET request.
+    if request.method == 'GET':
+        context['form'] = RegistrationForm()
+        return render(request, 'socialnetwork/register.html', context)
+
+    # Creates a bound form from the request POST parameters and makes the 
+    # form available in the request context dictionary.
+    form = RegistrationForm(request.POST)
+    context['form'] = form
+
+    # Validates the form.
+    if not form.is_valid():
+        return render(request, 'socialnetwork/register.html', context)
+
+    # At this point, the form data is valid.  Register and login the user.
+    new_user = User.objects.create_user(username=form.cleaned_data['username'], 
+                                        password=form.cleaned_data['password1'],
+                                        email=form.cleaned_data['email'],
+                                        first_name=form.cleaned_data['first_name'],
+                                        last_name=form.cleaned_data['last_name'])
+    # Mark the user as inactive to prevent login before email confirmation.
+    new_user.is_active = False
+    new_user.save()
+
+    # Create profile for user
+    new_profile = Profile(user=new_user)
+    new_profile.save()
+
+    # Generate a one-time use token and an email message body
+    token = default_token_generator.make_token(new_user)
+
+    email_body = """
+    Please click the link below to verify your email address and
+    complete the registration of your account:
+
+      http://{host}{path}
+    """.format(host=request.get_host(),
+               path=reverse('confirm', args=(new_user.username, token)))
+
+    send_mail(subject="Verify your email address",
+              message=email_body,
+              from_email="sisir@andrew.cmu.edu",
+              recipient_list=[new_user.email])
+
+    context['email'] = form.cleaned_data['email']
+
+    return render(request, 'socialnetwork/needs-confirmation.html', context)
+
+    # # Logs in the new user and redirects to homepage
+    # new_user = authenticate(username=form.cleaned_data['username'],
+    #                         password=form.cleaned_data['password1'])
+    # login(request, new_user)
+    # return redirect(reverse('home'))
+
+
+@transaction.atomic
+def confirm_registration(request, username, token):
+    user = get_object_or_404(User, username=username)
+
+    # Send 404 error if token is invalid
+    if not default_token_generator.check_token(user, token):
+        raise Http404
+
+    # Otherwise token was valid, activate the user.
+    user.is_active = True
+    user.save()
+
+    return render(request, 'socialnetwork/confirmed.html', {})
+
+
+
+@login_required
+@ensure_csrf_cookie
+def follower (request):
+    # get user's following list
+    curr_profile = get_object_or_404(Profile,user=request.user)
+    follows = curr_profile.following.all()
+    # get post of users the current user is following
+    posts = Post.objects.filter(created_by__in=follows).order_by("-creation_time")
+    comments = Comment.objects.all().order_by("-created_time")
+
+    context = {'posts': posts, 'user': request.user,'comments':comments}
+    return render(request, 'socialnetwork/follower.html', context)
+
+
+@login_required
+def profile(request, username):
+    profile_user = get_object_or_404(User, username=username)
+    request_user_profile = get_object_or_404(Profile, user=request.user)
+    curr_profile = get_object_or_404(Profile, user=profile_user)
+
+    follows = request_user_profile.following.all();
+    context = {'user' : profile_user, 'profile': curr_profile,  'follows': follows}
+
+    # The profile is for the logged-in user
+    if request.user.username == username:
+        return render(request, 'socialnetwork/editProfile.html', context)
+
+    # The profile page is for other users
+    return render(request, 'socialnetwork/profile.html', context)
+
+
+@login_required
+@transaction.atomic
+def edit(request):
+
+    curr_profile = get_object_or_404(Profile, user=request.user)
+    follows = curr_profile.following.all();
+
+    if request.method == 'GET':
+        form = ProfileForm()
+        context = {'profile': curr_profile, 'form': form, 'follows': follows}
+
+    else:
+        curr_profile = Profile.objects.select_for_update().get(user=request.user)
+        form = ProfileForm(request.POST, request.FILES)
+
+        if not form.is_valid():
+            context = {'profile': curr_profile, 'form': form, 'follows': follows}
+
+        else:
+            # Set update user info, and save it!
+            curr_profile.bio = form.cleaned_data['bio']
+            if form.cleaned_data['picture']:
+                curr_profile.picture = form.cleaned_data['picture']
+                curr_profile.content_type = form.cleaned_data['picture'].content_type
+            curr_profile.save();
+            context = {'profile': curr_profile, 'form': form, 'follows': follows}
+
+    return render(request, 'socialnetwork/editProfile.html', context)
+
+
+@login_required
+def get_photo(request, username):
+    # get user
+    user = get_object_or_404(User, username=username)
+    curr_profile = get_object_or_404(Profile, user=user)
+
+    if not curr_profile.picture:
+        raise Http404
+    return HttpResponse(curr_profile.picture, content_type=curr_profile.content_type)
+
+@login_required
+@ensure_csrf_cookie
+def post(request):
+    posts = Post.objects.all().order_by("-creation_time")
+    comments = Comment.objects.all().order_by("-created_time")
+
+    # no update if it is a get request
+    if request.method == 'GET':
+        context = {'posts': posts, 'form': PostForm(), 'comments':comments}
+        return render(request, 'socialnetwork/global.html', context)
+
+    form = PostForm(request.POST)
+    # validate form.
+    if not form.is_valid():
+        context = {'posts': posts, 'form': form, 'comments':comments}
+        return render(request, 'socialnetwork/global.html', context)
+
+    # create a new post if valid
+    new_post = Post(text=form.cleaned_data['text'], created_by=request.user, creation_time = timezone.now())
+    new_post.save()
+
+    posts = Post.objects.all().order_by("-creation_time")
+    context = {'posts': posts, 'form': PostForm(),'comments':comments}
+    return render(request, 'socialnetwork/global.html', context)
+
+
+@login_required
+@transaction.atomic
+def follow(request, username):
+    # follow the profile user
+    profile_user = get_object_or_404(User, username=username)
+    curr_user_profile = Profile.objects.get(user=request.user)
+    curr_user_profile.following.add(profile_user);
+    curr_user_profile.save()
+
+    # get return data
+    profile_user_profile = Profile.objects.get(user=profile_user)
+    follows = curr_user_profile.following.all()
+
+    context = {'user': profile_user, 'profile': profile_user_profile, 'follows': follows}
+    return render(request, 'socialnetwork/profile.html', context)
+
+
+@login_required
+@transaction.atomic
+def unfollow(request, username):
+    # follow the profile user
+    profile_user = get_object_or_404(User, username=username)
+    curr_user_profile = Profile.objects.get(user=request.user)
+    curr_user_profile.following.remove(profile_user);
+    curr_user_profile.save()
+
+    # get return data
+    profile_user_profile = Profile.objects.get(user=profile_user)
+    follows = curr_user_profile.following.all()
+
+    context = {'user': profile_user, 'profile': profile_user_profile, 'follows': follows}
+    return render(request, 'socialnetwork/profile.html', context)
+
+
+@login_required
+def getchanges(request,time):
+    # print "time",time
+    posts = Post.objects.filter(creation_time__gt=time).distinct().order_by("creation_time")
+    response_text = serializers.serialize('json', posts, use_natural_foreign_keys = True)
+    # print response_text;
+    return HttpResponse(response_text, content_type='application/json')
+
+@login_required
+def getchanges_follower(request,time,username):
+    curr_user = get_object_or_404(User, username=username)
+    curr_profile = get_object_or_404(Profile, user=curr_user)
+    follows = curr_profile.following.all()
+    posts = Post.objects.filter(created_by__in=follows, creation_time__gt=time).distinct().order_by("creation_time")
+
+    response_text = serializers.serialize('json', posts, use_natural_foreign_keys=True)
+    return HttpResponse(response_text, content_type='application/json')
+
+@login_required
+@transaction.atomic
+def add_comment(request,post_id):
+    if request.method != 'POST':
+        raise Http404
+
+    text = request.POST['text']
+    curr_post = Post.objects.get(id=post_id)
+
+    username = request.POST['username']
+    curr_user = get_object_or_404(User, username=username)
+
+    # Create a new comment
+    new_comment = Comment(content=text, created_by=curr_user, created_time=timezone.now(), post=curr_post)
+    new_comment.save()
+
+    return HttpResponse('', content_type='application/json')
+
+
+@login_required
+def get_comments_changes(request, max_pk, post_id):
+    curr_post = Post.objects.get(id=post_id)
+    response_text=[]
+    if curr_post:
+        comments = Comment.objects.filter(post=curr_post, id__gt=max_pk).distinct().order_by("created_time")
+        response_text = serializers.serialize('json', comments, use_natural_foreign_keys=True)
+    return HttpResponse(response_text, content_type='application/json')
+
